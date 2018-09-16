603e6f1246f42825d04b9a43712723a091644801
Ariel Adonai Souza <arielrenostro@gmail.com>
2018-09-16 15:59:48 -0300

    Exception

---
+++ src/br/furb/grafos/trabalho1/exception/MatrizAdjacenciaException.java
@@ -0,0 +1,15 @@
+package br.furb.grafos.trabalho1.exception;
+
+/**
+ *
+ * @author ariel, sidnei
+ *
+ */
+public class MatrizAdjacenciaException extends Exception {
+
+	private static final long serialVersionUID = -6804166606206282524L;
+
+	public MatrizAdjacenciaException(String msg) {
+		super(msg);
+	}
+}
